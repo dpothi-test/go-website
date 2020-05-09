76b29dcd1fe4612a6500e1303eb7c28e7a190256
Sudershan Pothina <Sdrshn-Home@sudershans-mbp.lan>
2020-05-09 16:29:28 -0500

    adding a simple router

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Page visited ")
-	fmt.Fprint(w, "test page")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Main Page </h1>")
+	} else if r.URL.Path == "/contact" {
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, "Contact Info: <a href=\"test@gmail.com\">link</a>")
+	}
 }
 func main() {
 	http.HandleFunc("/", handlerFunc)
